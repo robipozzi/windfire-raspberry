90164e46c9853caba8e1df3ff3e03c87baf0a990
Roberto Pozzi <r.robipozzi@gmail.com>
2021-03-18 16:43:35 +0100

    update script to ssh into Raspberry box

---
+++ raspberry-ssh.sh
@@ -0,0 +1,61 @@
+source setenv.sh
+
+###### Variable section - START
+OPTION=$1
+DESTINATION=
+SCRIPT="raspberry-ssh.sh"
+###### Variable section - END
+
+############
+## sshRun ##
+############
+sshRun()
+{
+	ssh pi@$DESTINATION -i /Users/robertopozzi/.ssh/ansible_rsa
+}
+
+###############
+## printHelp ##
+###############
+printHelp()
+{
+	printf "${red}Missing IP or Hostname destination to copy SSH key${end}\n"
+  	printf "${cyn}Usage: ./$SCRIPT <DESTINATION>${end}\n"
+    printf "\n"
+    printf "${cyn}Example: ./$SCRIPT 192.168.4.290${end}\n"
+	exit
+}
+
+printSelectDestination()
+{
+	echo ${grn}Select Raspberry Box platform to connect: ${end}
+	echo "${grn}1. raspberry1${end}"
+    echo "${grn}2. raspberry2${end}"
+	read OPTION
+	setDestination
+}
+
+setDestination()
+{
+	case $OPTION in
+		1)  DESTINATION="raspberry1"
+			;;
+        2)  DESTINATION="raspberry2"
+			;;
+		*) 	printf "\n${red}No valid option selected${end}\n"
+			printSelectDestination
+			;;
+	esac
+}
+
+# ##############################################
+# #################### MAIN ####################
+# ##############################################
+# ************ START evaluate args ************"
+if [ "$1" != " " ]; then
+	echo "Set Destination"
+    setDestination
+fi
+# ************** END evaluate args **************"
+RUN_FUNCTION=sshRun
+$RUN_FUNCTION
--- raspberry.sh
+++
@@ -1,2 +0,0 @@
-ssh pi@raspberry1 -i /Users/robertopozzi/.ssh/ansible_rsa
-#ssh pi@raspberry2 -i /Users/robertopozzi/.ssh/ansible_rsa
